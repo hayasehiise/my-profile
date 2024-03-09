bf46e15b07bee2c49627ed18caea3fe6923457a7
hayasehiise <kuronegahiise@gmail.com>
2024-03-09 17:37:34 +0800

    Fixed Some Margin

--- components/my_component/sectionOne.tsx
+++ components/my_component/sectionOne.tsx
@@ -7,7 +7,7 @@ import { motion } from "framer-motion";
 const SectionOne = () => {
     return(
         <>
-        <section className="flex-auto h-screen sm:h-full mb-96 sm:mb-0">
+        <section className="flex-auto h-screen sm:h-full mb-72 sm:mb-0">
           <div className="flex flex-col sm:flex-row w-full sm:h-full justify-center items-center">
             <ProfileCard />
             <SectionOneDetail />
