e183d5e6c3ce71c144a92eae44c6f1f9ece3466f
hayasehiise <kuronegahiise@gmail.com>
2024-03-09 19:43:20 +0800

    Nambah Link WA

--- components/my_component/section_component/contactButton.tsx
+++ components/my_component/section_component/contactButton.tsx
@@ -2,6 +2,7 @@
 
 import { Button } from "@/components/ui/button";
 import { motion } from "framer-motion";
+import Link from "next/link";
 
 const ButtonContactMe = () => {
   return (
@@ -11,10 +12,12 @@ const ButtonContactMe = () => {
         animate={{ opacity: 1 }}
         transition={{ duration: 0.5, delay: 6 }}
       >
+        <Link href="https://wa.me/6285343716854/?text=Hallo,%20Saya%20tertarik%20dengan%20website%20anda/">
         <Button variant="outline">
           Contact Me
           <img src="./icons/contact.svg" className="h-7" />
         </Button>
+        </Link>
       </motion.div>
     </>
   );
